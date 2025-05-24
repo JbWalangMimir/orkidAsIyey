fab591b384a2615c9dcd992dd31e972bb177a335
JbWalangMimir <jbbontogon1@gmail.com>
2025-05-24 18:05:14 +0800

    Update pages.js

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const pages = [
-  { title: "Grammatophyllum multiflorum", url: "Orchidaceae.html", tags: ["Orchid"] },
-  { title: "Model Specifics", url: "Model.html", tags: ["AI", "Model", "CNN"] },
+  { title: "Phalaenopsis philippinensis", url: "Phalaenopsis philippinensis.html", tags: ["Philippinensis", "philippinensis"] },
+  { title: "Grammatophyllum multiflorum", url: "Grammatophyllum multiflorum.html", tags: ["Grammatophyllum", "multiflorum"] },
   { title: "Phalaenopsis aphrodite", url: "Phalaenopsis aphrodite", tags: ["chemistry periodic"] },
   { title: "Vanda sanderiana", url: "Vanda sanderiana.html", tags: ["Vanda", "Waling-waling"] },
   { title: "Bulbophyllum lemniscatoides", url: "Orchid pages/Bulbophyllum lemniscatoides.html", tags: ["Bulbophyllum"] },
