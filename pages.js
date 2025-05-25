3f10a41d8843fa4ec32bff687f703283e3109bbf
JbWalangMimir <jbbontogon1@gmail.com>
2025-05-26 05:53:27 +0800

    Update pages.js

--- pages.js
+++ pages.js
@@ -3,6 +3,7 @@ const pages = [
   { title: "Aerides leeana", url: "Aerides leeana.html", tags: ["Aerides", "leeana"] },
   { title: "Amesiella monticola", url: "Amesiella monticola.html", tags: ["Amesiella", "monticola"] },
   { title: "Arundina graminifolia", url: "Arundina graminifolia.html", tags: ["Arundina", " graminifolia", "Bamboo Orchid"] },
+  { title: "Aphyllorchis montana", url: "Aphyllorchis montana.html", tags: ["Aphyllorchis", " montana"] },
   { title: "Grammatophyllum multiflorum", url: "Grammatophyllum multiflorum.html", tags: ["Grammatophyllum", "multiflorum"] },
   { title: "Phalaenopsis amabilis", url: "Phalaenopsis amabilis.html", tags: ["Phalaenopsis", " amabilis", "Moon orchid", "Moth orchid"] }, 
   { title: "Phalaenopsis philippinensis", url: "Phalaenopsis philippinensis.html", tags: ["Phalaenopsis", "philippinensis"] }, 
